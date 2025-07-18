aedd8b6bf8236fbc81de5c0ee720696fac677043
stepleonard <stephanie.leonard716@gmail.com>
2025-07-18 13:23:27 -0700

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+// These are the components imported. PersonCard Button ImageCard & button
 import PersonCard from "./components/PersonCard";
 import Button from "./components/Button.jsx";
 import ImageCard from "./components/ImageCard.jsx";
+// import images from components folder
 import image1 from "./components/image1.jpeg";
 import image2 from "./components/duckimg.jpg";
 import "./App.css";
 
+// make a function to handle the button click
 function handleClick() {
+  // console log that the button was clicked
   console.log("button clicked");
 }
-
+// make a function to display the personcard, imagecard and buttons
 function App() {
   return (
     <>
+      {/* render each person card */}
       <PersonCard name="Stephanie" age={21} occupation="Software Developer" />
       <PersonCard
         name="Franklin"
@@ -23,10 +28,15 @@ function App() {
         age={1}
         occupation="Listens to me while I code outloud"
       />
+      {/* renders button with click */}
       <Button text="Click Me" onClick={handleClick} />
+      {/* render button with different lable */}
       <Button text="2nd Button" onClick={handleClick} />
+      {/* make a div with a class for style the imagecards */}
       <div className="image-grid">
+        {/* renders frankies pic */}
         <ImageCard src={image1} alt="Bulldog Frankie" />
+        {/* renders lil duck pic */}
         <ImageCard src={image2} alt="My little rubber ducky" />
       </div>
     </>
